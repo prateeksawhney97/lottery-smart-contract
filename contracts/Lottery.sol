5071569f9929c18a11f26cbb50123949bfdee03b
Max Li <maxlivinci@gmail.com>
2018-01-12 15:19:05 +0800

    add enter function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -7,4 +7,9 @@ contract Lottery {
     function Lottery() public {
         manager = msg.sender;
     }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(msg.sender);
+    }
 }
