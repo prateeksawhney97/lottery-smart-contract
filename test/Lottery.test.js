99b2561efb03d196a99756663492085ecd095261
Max Li <maxlivinci@gmail.com>
2018-03-02 17:08:10 +0800

    test asserting multiple players

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -33,4 +33,31 @@ describe('Lottery', () => {
     expect(accounts[0]).toEqual(players[0])
     expect(1).toEqual(players.length)
   });
+
+
+  it('allows multiple accounts to enter', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    await lottery.methods.enter().send({
+      from: accounts[2],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    expect(accounts[0]).toEqual(players[0])
+    expect(accounts[1]).toEqual(players[1])
+    expect(accounts[2]).toEqual(players[2])
+    expect(3).toEqual(players.length)
+  });
 });
